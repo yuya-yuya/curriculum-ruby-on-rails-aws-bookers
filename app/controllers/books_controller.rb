512bc1ab1b436626c902c50ca6c2a67ef14874e4
yuya-yuya <yytyfp5115@icloud.com>
2021-01-06 12:08:29 +0000

    additional commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,47 @@
 class BooksController < ApplicationController
+  def top
+  end
+  
   def index
     @book = Book.new
     @books = Book.all
   end
-  
+
   def new
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book)
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    redirect_to book_path(book.id)
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
     @book = Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
